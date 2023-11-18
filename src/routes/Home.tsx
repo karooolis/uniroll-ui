34d7aea3d313c2ce3ed0fcffe921abec1605aa9d
karooolis <hello@karolisram.com>
2023-11-18 18:18:30 +0300

    style: add headers

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
+      <h2 className="my-8 text-2xl">Welcome!</h2>
+
       <Button className="mr-3" asChild>
         <Link to="/create">Create</Link>
       </Button>
